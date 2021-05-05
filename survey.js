c0252d6cba1659e2ff58e0613b823636a51a7b6b
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-04 21:59:16 -0700

    added paragraph console.logs

--- survey.js
+++ survey.js
@@ -5,20 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What is your name? Nicknames are also acceptable ', (answer) => {
-  console.log(`Hello, ${answer}!`)
+rl.question('What is your name? Nicknames are also acceptable ', (name) => {
+  console.log(`Hello, ${name}!`)
   
-  rl.question("What do you like to do on your free time? ", (answer) => {
-    console.log(`You like to ${answer}. That's awesome!`)
+  rl.question("What do you like to do on your free time? ", (hobby) => {
+    console.log(`You like to ${hobby}. That's awesome!`)
     
-    rl.question("What music genre do you like? ", (answer) => {
-      console.log(`You like to listen to ${answer}. That's cool!`)
+    rl.question("What music genre do you like? ", (music) => {
+      console.log(`You like to listen to ${music}. That's cool!`)
 
-      rl.question("What is your favourite food? ", (answer) => {
-        console.log(`You like to eat ${answer}. Yum!`)
+      rl.question("What is your favourite food? ", (food) => {
+        console.log(`You like to eat ${food}. Yum!`)
         
+        console.log(`My name is ${name}. My hobby is ${hobby}. I like to listen to ${music}. I like to eat ${food}.`);
+
         rl.close();
-        
       });
     });
   });
